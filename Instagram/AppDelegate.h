bc65071a3563d6a33d1231bb26b68f89336a5e24
Eva Xie <xieyi@fb.com>
2021-07-06 14:13:16 -0400

    first commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Eva Xie on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
